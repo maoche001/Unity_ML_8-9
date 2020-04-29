ce0a6eef55229803461751b0273095ece64003a7
maoche <maoche001@gmail.com>
2020-04-29 14:31:00 +0800

    完成

---
+++ Unity_ML_8+9/Assets/GunAgent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+
+public class GunAgent : Agent
+{
+    public float speed = 10;
+    private Rigidbody rigRobot;
+    private Rigidbody rigGun;
+    private Rigidbody rigEnemy;
+    private int countTotal;
+    private int countComplete;
+    public GameObject[] bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigRobot = GetComponent<Rigidbody>();
+        rigGun = GameObject.Find("gun").GetComponent<Rigidbody>();
+        rigEnemy = GameObject.Find("enemy").GetComponent<Rigidbody>();
+    }
+    public override void OnEpisodeBegin()
+    {
+        countTotal++;
+
+        rigRobot.velocity = Vector3.zero;
+        rigRobot.angularVelocity = Vector3.zero;
+        rigGun.velocity = Vector3.zero;
+        rigGun.angularVelocity = Vector3.zero;
+        rigEnemy.velocity = Vector3.zero;
+        rigEnemy.angularVelocity = Vector3.zero;
+
+
+        
+        Vector3 posRobot = new Vector3(Random.Range(-5, 5), 5,-11);
+        transform.position = posRobot;
+        Vector3 posGun = new Vector3(posRobot.x, 5.45f, -9.675f);
+        rigGun.position = posGun;
+        Vector3 posEnemy = new Vector3(Random.Range(-5, 5), 5, 0);
+        rigEnemy.position = posEnemy;
+        enemy.complete = false;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(rigGun.position);
+        sensor.AddObservation(rigGun.velocity);
+        sensor.AddObservation(rigEnemy.position);
+    }
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.z = vectorAction[0];
+        controlSignal.x = vectorAction[1];
+        rigGun.AddForce(controlSignal * speed);
+
+        if (enemy.complete)
+        {
+            countComplete++;
+            SetReward(1);
+            EndEpisode();
+        }
+        if (rigGun.position.z > 4|| rigGun.position.z <-10)
+        {
+            SetReward(-1);
+            EndEpisode();
+        }
+    }
+}
---
+++ Unity_ML_8+9/Assets/enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public static bool complete;
+    public GameObject eny;
+    public GameObject gun;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.name == "gun")
+        {
+            complete = true;
+            //Destroy(eny);
+            //Destroy(gun);
+        }
+    }
+}
